99278f5501397c645969e23e544ea429bd48481c
vss <vss@csail.mit.edu>
2017-11-05 21:02:16 -0500

    training data generator

--- lstm_baseline.py
+++ lstm_baseline.py
@@ -17,7 +17,7 @@ from nltk.tokenize import RegexpTokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 np.random.seed(0)
-
+#torch.manual_seed(0)
 
 DATA_PATH = '/data/vision/fisher/data1/vsmolyakov/nlp_project/data/askubuntu/'
 
@@ -94,6 +94,7 @@ ax1.set_title('title length histogram'); ax1.legend(loc=1);
 ax2.set_title('body length histogram'); ax2.legend(loc=1);
 plt.savefig('./figures/question_len_hist.png')
 
+"""
 print "fitting tf-idf vectorizer..."
 tic = time()
 tfidf = TfidfVectorizer(tokenizer=tokenizer.tokenize, analyzer='word', ngram_range=(1,1))
@@ -104,7 +105,54 @@ print "elapsed time: %.2f sec" %(toc - tic)
 vocab = tfidf.vocabulary_
 print "vocab size: ", len(vocab)
 print "embeddings size: ", embeddings.shape
+"""
 
+print "generating training, validation, test datasets..."
+tic = time()
+train_data = []
+for idx in tqdm(range(train_idx_df.shape[0])):
+    query_id = train_idx_df.loc[idx, 'query_id']
+    similar_id_list = map(int, train_idx_df.loc[idx, 'similar_id'].split(' '))
+    random_id_list = map(int, train_idx_df.loc[idx, 'random_id'].split(' '))
+    
+    #query title and body tensor ids
+    query_title = train_text_df[train_text_df['id'] == query_id].title.tolist() 
+    query_body = train_text_df[train_text_df['id'] == query_id].body.tolist()
+    query_title_tokens = tokenizer.tokenize(query_title[0])[:MAX_TITLE_LEN]
+    query_body_tokens = tokenizer.tokenize(query_body[0])[:MAX_BODY_LEN]
+    query_title_tensor_idx = get_tensor_idx(query_title_tokens, word_to_idx, MAX_TITLE_LEN) 
+    query_body_tensor_idx = get_tensor_idx(query_body_tokens, word_to_idx, MAX_BODY_LEN) 
 
+    for similar_id in similar_id_list:
+        sample = {}  #reset sample dictionary here
+        sample['query_title'] = query_title_tensor_idx
+        sample['query_body'] = query_body_tensor_idx
 
+        similar_title = train_text_df[train_text_df['id'] == similar_id].title.tolist() 
+        similar_body = train_text_df[train_text_df['id'] == similar_id].body.tolist()
+        similar_title_tokens = tokenizer.tokenize(similar_title[0])[:MAX_TITLE_LEN]
+        similar_body_tokens = tokenizer.tokenize(similar_body[0])[:MAX_BODY_LEN]
+        similar_title_tensor_idx = get_tensor_idx(similar_title_tokens, word_to_idx, MAX_TITLE_LEN) 
+        similar_body_tensor_idx = get_tensor_idx(similar_body_tokens, word_to_idx, MAX_BODY_LEN)
+        sample['similar_title'] = similar_title_tensor_idx
+        sample['similar_body'] = similar_body_tensor_idx
 
+        for ridx, random_id in enumerate(random_id_list):
+            random_title_name = 'random_title_' + str(ridx)
+            random_body_name = 'random_body_' + str(ridx)
+        
+            random_title = train_text_df[train_text_df['id'] == random_id].title.tolist() 
+            random_body = train_text_df[train_text_df['id'] == random_id].body.tolist()
+            random_title_tokens = tokenizer.tokenize(random_title[0])[:MAX_TITLE_LEN]
+            random_body_tokens = tokenizer.tokenize(random_body[0])[:MAX_BODY_LEN]
+            random_title_tensor_idx = get_tensor_idx(random_title_tokens, word_to_idx, MAX_TITLE_LEN) 
+            random_body_tensor_idx = get_tensor_idx(random_body_tokens, word_to_idx, MAX_BODY_LEN)
+            sample[random_title_name] = random_title_tensor_idx
+            sample[random_body_name] = random_body_tensor_idx
+        #end for
+        train_data.append(sample)
+    #end for
+#end for
+
+toc = time()
+print "elapsed time: %.2f sec" %(toc - tic)
