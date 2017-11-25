83981433e30364c322a641063f7c9fba8988f4fb
vss <vss@csail.mit.edu>
2017-11-25 00:11:41 -0500

    direct domain transfer

---
+++ src/domain_transfer_direct_cnn.py
@@ -0,0 +1,283 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import ConfigParser
+from tqdm import tqdm
+from time import time
+import cPickle as pickle
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics.pairwise import cosine_similarity
+
+np.random.seed(0)
+
+DATA_PATH_SOURCE = '/data/vision/fisher/data1/vsmolyakov/nlp_project/data/askubuntu/'
+DATA_PATH_TARGET = '/data/vision/fisher/data1/vsmolyakov/nlp_project/data/android/'
+
+tokenizer = RegexpTokenizer(r'\w+')
+stop = set(stopwords.words('english'))
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(r'config.ini'))
+SAVE_PATH = config.get('paths', 'save_path')
+RNN_SAVE_NAME = config.get('rnn_params', 'save_name')
+CNN_SAVE_NAME = config.get('cnn_params', 'save_name')
+EMBEDDINGS_FILE = config.get('paths', 'embeddings_path')
+MAX_TITLE_LEN = int(config.get('data_params', 'MAX_TITLE_LEN'))
+MAX_BODY_LEN = int(config.get('data_params', 'MAX_BODY_LEN'))
+#TODO: do we keep the same title and body len for android dataset?
+
+def get_embeddings():
+    lines = []
+    with open(EMBEDDINGS_FILE, 'r') as f:
+        lines = f.readlines()
+        f.close()
+    
+    embedding_tensor = []
+    word_to_idx = {}
+    
+    for idx, l in enumerate(lines):
+        word, emb = l.split()[0], l.split()[1:]
+        vector = [float(x) for x in emb]
+        if idx == 0: #reserved
+            embedding_tensor.append(np.zeros(len(vector)))
+        embedding_tensor.append(vector)
+        word_to_idx[word] = idx+1
+    #end for
+    embedding_tensor = np.array(embedding_tensor, dtype=np.float32)    
+    return embedding_tensor, word_to_idx
+        
+def get_tensor_idx(text, word_to_idx, max_len):
+    null_idx = 0  #idx if word is not in the embeddings dictionary
+    text_idx = [word_to_idx[x] if x in word_to_idx else null_idx for x in text][:max_len]
+    if len(text_idx) < max_len:
+        text_idx.extend([null_idx for _ in range(max_len - len(text_idx))])    
+    x = torch.LongTensor(text_idx)  #64-bit integer
+    return x
+        
+def generate_data(data_frame, train_text_df, word_to_idx, tokenizer):
+
+    dataset = []
+    for idx in tqdm(range(data_frame.shape[0])):
+    #for idx in tqdm(range(1000)):
+        q1_id = data_frame.loc[idx, 'id_1']
+        q2_id = data_frame.loc[idx, 'id_2']
+
+        #q1 title and body tensor ids
+        q1_title = train_text_df[train_text_df['id'] == q1_id].title.tolist() 
+        q1_body = train_text_df[train_text_df['id'] == q1_id].body.tolist()
+        q1_title_tokens = tokenizer.tokenize(q1_title[0])[:MAX_TITLE_LEN]
+        q1_body_tokens = tokenizer.tokenize(q1_body[0])[:MAX_BODY_LEN]
+        q1_title_tensor_idx = get_tensor_idx(q1_title_tokens, word_to_idx, MAX_TITLE_LEN) 
+        q1_body_tensor_idx = get_tensor_idx(q1_body_tokens, word_to_idx, MAX_BODY_LEN)
+
+        #q2 title and body tensor ids
+        q2_title = train_text_df[train_text_df['id'] == q2_id].title.tolist() 
+        q2_body = train_text_df[train_text_df['id'] == q2_id].body.tolist()
+        q2_title_tokens = tokenizer.tokenize(q2_title[0])[:MAX_TITLE_LEN]
+        q2_body_tokens = tokenizer.tokenize(q2_body[0])[:MAX_BODY_LEN]
+        q2_title_tensor_idx = get_tensor_idx(q2_title_tokens, word_to_idx, MAX_TITLE_LEN) 
+        q2_body_tensor_idx = get_tensor_idx(q2_body_tokens, word_to_idx, MAX_BODY_LEN)
+
+        sample = {}  #reset sample dictionary here
+        sample['q1_idx'] = q1_id
+        sample['q1_title'] = q1_title_tensor_idx
+        sample['q1_body'] = q1_body_tensor_idx
+
+        sample['q2_idx'] = q2_id
+        sample['q2_title'] = q2_title_tensor_idx
+        sample['q2_body'] = q2_body_tensor_idx
+
+        dataset.append(sample)
+    #end for
+    return dataset 
+
+#load data
+print "loading data..."
+tic = time()
+target_text_file = DATA_PATH_TARGET + '/corpus.tsv'
+target_text_df = pd.read_table(target_text_file, sep='\t', header=None)
+target_text_df.columns = ['id', 'title', 'body']
+target_text_df = target_text_df.dropna()
+target_text_df['title'] = target_text_df['title'].apply(lambda words: ' '.join(filter(lambda x: x not in stop, words.split())))
+target_text_df['body'] = target_text_df['body'].apply(lambda words: ' '.join(filter(lambda x: x not in stop, words.split())))
+target_text_df['title_len'] = target_text_df['title'].apply(lambda words: len(tokenizer.tokenize(str(words))))
+target_text_df['body_len'] = target_text_df['body'].apply(lambda words: len(tokenizer.tokenize(str(words))))
+
+target_pos_file = DATA_PATH_TARGET + '/test.pos.txt'
+target_pos_df = pd.read_table(target_pos_file, sep=' ', header=None)
+target_pos_df.columns = ['id_1', 'id_2']
+
+target_neg_file = DATA_PATH_TARGET + '/test.neg.txt'
+target_neg_df = pd.read_table(target_neg_file, sep=' ', header=None)
+target_neg_df.columns = ['id_1', 'id_2']
+toc = time()
+print "elapsed time: %.2f sec" %(toc - tic)
+
+
+print "loading embeddings..."
+tic = time()
+embeddings, word_to_idx = get_embeddings()
+print "vocab size (embeddings): ", len(word_to_idx)
+toc = time()
+print "elapsed time: %.2f sec" %(toc - tic)
+
+print "generating test pos and neg datasets..."
+tic = time()
+target_pos_data = generate_data(target_pos_df, target_text_df, word_to_idx, tokenizer)
+target_neg_data = generate_data(target_neg_df, target_text_df, word_to_idx, tokenizer)
+toc = time()
+print "elapsed time: %.2f sec" %(toc - tic)
+
+print "loading CNN model pre-trained on source dataset..."
+#CNN architecture
+class  CNN(nn.Module):
+    def __init__(self, embed_num, embed_dim, kernel_num, kernel_sizes):
+        super(CNN,self).__init__()
+        V = embed_num
+        D = embed_dim
+        Ci = 1            #input channel
+        Co = kernel_num   #depth
+        Ks = kernel_sizes #height of each filter
+
+        self.embed = nn.Embedding(V, D)
+        self.embed.weight.data = torch.from_numpy(embeddings)
+        self.convs1 = nn.ModuleList([nn.Conv2d(Ci, Co, (K, D)) for K in Ks])
+
+    def forward(self, x):
+        x = self.embed(x) # (N,W,D)
+        x = x.unsqueeze(1) # (N,Ci,W,D)
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1] #[(N,Co,W), ...]*len(Ks)
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x] #[(N,Co), ...]*len(Ks)
+        x = torch.cat(x, 1)
+        return x
+
+#CNN parameters
+batch_size = 32
+embed_num = len(word_to_idx)
+embed_dim = len(embeddings[0])
+kernel_num = 100  
+kernel_sizes = range(2,6)
+model = CNN(embed_num, embed_dim, kernel_num, kernel_sizes)
+
+#CNN weights
+model = torch.load(SAVE_PATH + CNN_SAVE_NAME)
+
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    print "found CUDA GPU..."
+    model = model.cuda()
+
+model.eval()
+
+print "scoring similarity between target questions..."
+y_true, y_pred_cnn = [], []
+
+test_data_loader_pos = torch.utils.data.DataLoader(
+    target_pos_data, 
+    batch_size = batch_size,
+    shuffle = False,
+    num_workers = 4, 
+    drop_last = True)
+        
+for batch in tqdm(test_data_loader_pos):
+
+    q1_idx = batch['q1_idx']
+    q1_title = Variable(batch['q1_title'])
+    q1_body = Variable(batch['q1_body'])
+
+    q2_idx = batch['q2_idx']
+    q2_title = Variable(batch['q2_title'])
+    q2_body = Variable(batch['q2_body'])
+
+    if use_gpu:
+        q1_title, q1_body = q1_title.cuda(), q1_body.cuda()
+        q2_title, q2_body = q2_title.cuda(), q2_body.cuda()
+
+    #q1
+    cnn_q1_title = model(q1_title)
+    cnn_q1_body = model(q1_body)
+    cnn_q1 = (cnn_q1_title + cnn_q1_body)/2.0
+
+    #q2 
+    cnn_q2_title = model(q2_title)
+    cnn_q2_body = model(q2_body)
+    cnn_q2 = (cnn_q2_title + cnn_q2_body)/2.0
+
+    cosine_similarity = nn.CosineSimilarity(dim=1, eps=1e-6)
+    score_pos = cosine_similarity(cnn_q1, cnn_q2)
+
+    score_pos_numpy = score_pos.cpu().data.numpy() #TODO: check (some scores are negative)
+
+    y_true.extend(np.ones(batch_size)) #true label (similar)
+    y_pred_cnn.extend(score_pos_numpy.tolist())
+#end for        
+
+test_data_loader_neg = torch.utils.data.DataLoader(
+    target_neg_data, 
+    batch_size = batch_size,
+    shuffle = False,
+    num_workers = 4, 
+    drop_last = True)
+        
+for batch in tqdm(test_data_loader_neg):
+
+    q1_idx = batch['q1_idx']
+    q1_title = Variable(batch['q1_title'])
+    q1_body = Variable(batch['q1_body'])
+
+    q2_idx = batch['q2_idx']
+    q2_title = Variable(batch['q2_title'])
+    q2_body = Variable(batch['q2_body'])
+
+    if use_gpu:
+        q1_title, q1_body = q1_title.cuda(), q1_body.cuda()
+        q2_title, q2_body = q2_title.cuda(), q2_body.cuda()
+
+    #q1
+    cnn_q1_title = model(q1_title)
+    cnn_q1_body = model(q1_body)
+    cnn_q1 = (cnn_q1_title + cnn_q1_body)/2.0
+
+    #q2 
+    cnn_q2_title = model(q2_title)
+    cnn_q2_body = model(q2_body)
+    cnn_q2 = (cnn_q2_title + cnn_q2_body)/2.0
+
+    cosine_similarity = nn.CosineSimilarity(dim=1, eps=1e-6)
+    score_neg = cosine_similarity(cnn_q1, cnn_q2)
+
+    score_neg_numpy = score_neg.cpu().data.numpy() #TODO: check (some scores are negative)
+
+    y_true.extend(np.zeros(batch_size)) #true label (not similar)
+    y_pred_cnn.extend(score_neg_numpy.tolist())
+#end for        
+
+roc_auc = roc_auc_score(y_true, y_pred_cnn)
+print "area under ROC curve: ", roc_auc
+
+fpr, tpr, thresholds = roc_curve(y_true, y_pred_cnn)
+
+plt.figure()
+plt.plot(fpr, tpr, c='b', lw=2.0, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], c='k', lw=2.0, linestyle='--')
+plt.title('CNN Domain Transfer Direct')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.legend(loc="lower right")
+plt.savefig('../figures/domain_transfer_direct_cnn.png')
+
+
+
