2f4e9cc18075f5db8dbabfbc570da71a8304f19b
vss <vss@csail.mit.edu>
2017-12-09 13:13:45 -0500

    latest

--- src/report_figures.py
+++ src/report_figures.py
@@ -16,6 +16,32 @@ filename = SAVE_PATH + 'figures_cnn.dat'
 with open(filename) as f: 
     figures_cnn = pickle.load(f)
 
+filename = SAVE_PATH + 'figures_lstm.dat' 
+with open(filename) as f: 
+    figures_lstm = pickle.load(f)
+
+filename = SAVE_PATH + 'figures_da_tfidf.dat' 
+with open(filename) as f: 
+    figures_da_tfidf = pickle.load(f)
+
+filename = SAVE_PATH + 'figures_da_cnn_direct.dat' 
+with open(filename) as f: 
+    figures_da_cnn_direct = pickle.load(f)
+
+filename = SAVE_PATH + 'figures_da_lstm_direct.dat' 
+with open(filename) as f: 
+    figures_da_lstm_direct = pickle.load(f)
 
 
 
+
+filename = SAVE_PATH + 'figures_da_doc2vec.dat' 
+with open(filename) as f: 
+    figures_da_doc2vec = pickle.load(f)
+
+filename = SAVE_PATH + 'figures_da_siamese.dat' 
+with open(filename) as f: 
+    figures_da_siamese = pickle.load(f)
+
+
+#generate plots
